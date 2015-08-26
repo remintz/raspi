8744e5c145571dd911582b10f1ffdf1d7b1c5b0d
Renato Mintz <renato.mintz@accenture.com>
2015-08-26 17:04:13 -0300

    Fixes

--- init.sh
+++ init.sh
@@ -305,7 +305,7 @@ if [ "$setupAdhocWifi" == "1" ]; then
 	echo -e "Adhoc connection will be set whenever not able to connect to SSID $ssid"
 	echo -e "The SSID for the adhoc connection is: $adhocSsid"
 	echo -e "The IP Address of the Pi will be: $adhocIpGroup.200"
-	echo -e "DHCP is enabled"
+	echo -e "DHCP is enabled\n\n\n\n\n\n"
 
 	if [ "$setupWifi" == "1" ]; then
 
@@ -316,11 +316,13 @@ if [ "$setupAdhocWifi" == "1" ]; then
 		sudo cp /etc/dhcp/dhcp.conf /etc/dhcp/dhcp.conf.bak
 		sudo cp /etc/rc.local /etc/rc.local.bak
 
-		sudo cat /etc/default/isc-dhcp-server | sed -e 's/INTERFACES=\"\"/INTERFACES=\"$interface\"/' > /etc/default/isc-dhcp-server
-		cat files/dhcpd.conf | sed -e "s/\#INTERFACE/$interface/" -e "s/\#ADHOC_SSID/$adhocSsid/" -e "s/\#ADHOC_IP_GROUP/$adhocIpGroup" > ./dhcp.conf
+		cat /etc/default/isc-dhcp-server | sed -e 's/INTERFACES=\"\"/INTERFACES=\"$interface\"/' > ./isc-dhcp-server
+		sudo mv ./isc-dhcp-server /etc/default/isc-dhcp-server
+
+		cat files/dhcpd.conf | sed -e "s/\#INTERFACE/$interface/" -e "s/\#ADHOC_SSID/$adhocSsid/" -e "s/\#ADHOC_IP_GROUP/$adhocIpGroup/" > ./dhcp.conf
 		sudo mv -f ./dhcp.conf /etc/dhcp/dhcpd.conf
 
-		cat files/rc.local | sed -e "s/\#INTERFACE/$interface/" -e "s/\#ADHOC_SSID/$adhocSsid/" -e "s/\#ADHOC_IP_GROUP/$adhocIpGroup" -e "s/\#SSID/$ssid" > ./rc.local
+		cat files/rc.local | sed -e "s/\#INTERFACE/$interface/" -e "s/\#ADHOC_SSID/$adhocSsid/" -e "s/\#ADHOC_IP_GROUP/$adhocIpGroup/" -e "s/\#SSID/$ssid/" > ./rc.local
 		sudo mv -f ./rc.local /etc/rc.local
 
 		sudo update-rc.d -f isc-dhcp-server disable 		# prevent dhcp server to start automatically
